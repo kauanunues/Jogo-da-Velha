05c33be163103298fee90926ef657a8e31f96649
kauanunes <marykakinho@gmail.com>
2024-08-05 10:53:39 -0300

    comitadin

--- jogo-da-velha/components/figura.js
+++ jogo-da-velha/components/figura.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { TouchableOpacity } from 'react-native'
-import { EnTypo } from '@expo/vector-icons'
+import { Entypo } from '@expo/vector-icons'
 
-export default function figura() {
+export default function Figura({ vetor, posicao, clicado }) {
+  const getIcon = () => {
+    if (vetor[posicao] !== 0) {
+      return vetor[posicao] ? "cross" : "circle";
+    }
+    return "pencil";
+  }
   return (
-    <TouchableOpacity>
-      <EnTypo name ="circle" size={100} color="#000"/>
+    <TouchableOpacity onPress={clicado}>
+      <Entypo name={getIcon()} size={100} color="#000" />
     </TouchableOpacity>
   )
 }
